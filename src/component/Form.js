8996dfdaa66980409c1104a0be21b544a411e3e1
sabeelmhd <sabeelzaim@gmail.com>
2024-12-07 01:00:34 +0530

    Updated email profile title

--- src/component/Form.js
+++ src/component/Form.js
@@ -5,15 +5,19 @@ import React from 'react'
 const Form = () => {
   return (
     <div className='form'>
-      <form>
+      <form action='https://formsubmit.co/sabeelzaim@gmail.com' method='POST'>
         <label>Your Name</label>
-        <input type='text'></input>
+        <input type='text' name='name'></input>
         <label>Email</label>
-        <input type='email'></input>
+        <input type='email' name='email'></input>
         <label>Subject</label>
-        <input type='text'></input>
+        <input type='text' name='subject'></input>
         <label>Message</label>
-        <textarea rows='6' placeholder='Type your message here' />
+        <textarea
+          rows='6'
+          name='message'
+          placeholder='Type your message here'
+        />
         <button className='btn'>Submit</button>
       </form>
     </div>
